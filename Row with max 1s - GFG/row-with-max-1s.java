797ac757aa97a87205dcb85ad47ddc80d1ffcc9c
Shanmukh Maddimsetti <140135125+saiganesh00@users.noreply.github.com>
2023-09-12 20:23:49 +0530

    Added solution - LeetHub

---
+++ Row with max 1s - GFG/row-with-max-1s.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+        
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int rowWithMax1s(int arr[][], int n, int m) {
+        // code here
+        int finalMax = 0;
+        int count = 0;
+        int index = 0;
+        for(int i = 0; i < n; i++){
+            count = 0;
+            for(int j = 0; j < m; j++){
+                if(arr[i][j] == 1){
+                    count += 1;
+                }
+            }
+            if(count > finalMax){
+                index = i;
+            }
+            finalMax = Math.max(finalMax, count);
+        }
+        if(finalMax == 0){
+            return -1;
+        }
+        return index;
+    }
+}
